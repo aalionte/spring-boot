1b0019ab1ee0d6b8dfd1547da58ef2618d9d75dd
Raluca Gimbuta <raluca.gimbuta@endava.com>
2015-09-01 10:38:58 +0300

    removed hibernate dependecy from pom.xml formatted AbstractDAO and ActorDAO removed duplicate columns in home.html

--- src/main/java/com/endava/movie/dao/AbstractDAO.java
+++ src/main/java/com/endava/movie/dao/AbstractDAO.java
@@ -10,43 +10,43 @@ import org.springframework.transaction.annotation.Transactional;
 @EnableTransactionManagement
 public abstract class AbstractDAO<E> {
 
-    @PersistenceContext
-    private EntityManager em;
+	@PersistenceContext
+	private EntityManager em;
 
-    private final Class<E> entityClass;
+	private final Class<E> entityClass;
 
-    protected AbstractDAO(final Class<E> entityClass) {
-        this.entityClass = entityClass;
-    }
+	protected AbstractDAO(final Class<E> entityClass) {
+		this.entityClass = entityClass;
+	}
 
-    @Transactional
-    public EntityManager em() {
-        return em;
-    }
+	@Transactional
+	public EntityManager em() {
+		return em;
+	}
 
-    @Transactional
-    public E findById(final Long entityId) {
-        return em.find(entityClass, entityId);
-    }
+	@Transactional
+	public E findById(final Long entityId) {
+		return em.find(entityClass, entityId);
+	}
 
-    @Transactional
-    public void save(final E entityToBeSaved) {
-        em.persist(em.merge(entityToBeSaved));
-    }
+	@Transactional
+	public void save(final E entityToBeSaved) {
+		em.persist(em.merge(entityToBeSaved));
+	}
 
-    @Transactional
-    public void update(final E entityToBeUpdated) {
-        em.merge(entityToBeUpdated);
-    }
+	@Transactional
+	public void update(final E entityToBeUpdated) {
+		em.merge(entityToBeUpdated);
+	}
 
-    @Transactional
-    public void remove(final E entityToBeRemoved) {
-        em.remove(entityToBeRemoved);
-    }
+	@Transactional
+	public void remove(final E entityToBeRemoved) {
+		em.remove(entityToBeRemoved);
+	}
 
-    @Transactional
-    public void delete(final Long id) {
-        em.remove(findById(id));
-    }
+	@Transactional
+	public void delete(final Long id) {
+		em.remove(findById(id));
+	}
 
 }
