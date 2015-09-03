7884b0065f3258c3fa8ce5e4036871d6930bbdaa
Raluca Gimbuta <raluca.gimbuta@endava.com>
2015-09-03 17:33:35 +0300

    Added a test.

--- src/test/java/demo/DemoApplicationTests.java
+++ src/test/java/demo/DemoApplicationTests.java
@@ -1,9 +1,13 @@
 package demo;
 
-import org.junit.Ignore;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -14,9 +18,13 @@ import com.endava.movie.DemoApplication;
 @WebAppConfiguration
 public class DemoApplicationTests {
 
-	// @Test
-	@Ignore
-	public void contextLoads() {
+	@Autowired
+	ApplicationContext ctx;
+
+	@Test
+	public void testContextLoads() throws Exception {
+		assertNotNull(this.ctx);
+		assertTrue(this.ctx.containsBean("demoApplication"));
 	}
 
 }
