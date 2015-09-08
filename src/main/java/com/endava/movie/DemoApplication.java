bde8f7ae8b805fec85691012da94c3573dade8d0
Raluca Gimbuta <raluca.gimbuta@endava.com>
2015-09-08 17:30:55 +0300

    added microinfra

--- src/main/java/com/endava/movie/DemoApplication.java
+++ src/main/java/com/endava/movie/DemoApplication.java
@@ -1,14 +1,37 @@
 package com.endava.movie;
+import static com.ofg.config.BasicProfiles.DEVELOPMENT;
+import static com.ofg.config.BasicProfiles.PRODUCTION;
+import static com.ofg.config.BasicProfiles.TEST;
 
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.ofg.infrastructure.config.EnableMicroservice;
+import com.ofg.infrastructure.environment.EnvironmentSetupVerifier;
+
 @SpringBootApplication
 @ComponentScan
+//@EnableMicroservice
 public class DemoApplication {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
+//		
+//		for(String arg : args){
+//			LOGGER.debug(arg);
+//		}
+////		
+//		SpringApplication application = new SpringApplication(DemoApplication.class);
+//        application.addListeners(new EnvironmentSetupVerifier(Arrays.asList(DEVELOPMENT, PRODUCTION, TEST)));
+//        application.run(args);
+//		
+		
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
---
+++ src/main/java/com/endava/movie/MicroinfraConfig.java
@@ -0,0 +1,10 @@
+package com.endava.movie;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ofg.infrastructure.web.exception")
+public class MicroinfraConfig {
+
+}
